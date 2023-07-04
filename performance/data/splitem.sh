2a95a9f8c288725413107cacb98b9626343bcb3b
Tom Marsh <ts.marsh@gmail.com>
2023-07-04 07:31:57 -0400

    test data for billing_account

--- performance/data/splitem.sh
+++ performance/data/splitem.sh
@@ -1,2 +1,2 @@
 counter=100
-jq -c '.[]' users.json | while read -r item; do echo "$item" > "user_${counter}.json"; let counter=counter+1; done
+jq -c '.[]' billing_account.json | while read -r item; do echo "$item" > "billing_account/billing_account_${counter}.json"; let counter=counter+1; done
